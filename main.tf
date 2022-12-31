71f412bfc166a8e5118fa05b0bebf7c53eb1e707
Mohd Sabir Rabbani Khan <107764809+Sabirk8992@users.noreply.github.com>
2023-01-01 01:33:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,21 +8,21 @@ data "http" "ip" {
 }
 
 # Create the S3 bucket
-resource "aws_s3_bucket" "mymy_bucket" {
+resource "aws_s3_bucket" "iaaciam" {
   bucket = "my-bucket"
   acl    = "public-read"
 }
 
 # Enable versioning for the S3 bucket
-resource "aws_s3_bucket" "mymy_buckets" {
-  bucket = "my-bucket"
+resource "aws_s3_bucket" "iaaciam1" {
+  bucket = "iaaciam"
   versioning {
     enabled = true
   }
 }
 
 # Create the IAM policy that allows bucket objects only from a specific whitelisted public IP
-resource "aws_iam_policy" "my_policy" {
+resource "aws_iam_policy" "iaaciamp" {
   name        = "my-policy"
   description = "IAM policy that allows bucket objects only from a specific whitelisted public IP"
 
@@ -33,7 +33,7 @@ resource "aws_iam_policy" "my_policy" {
     {
       "Effect": "Allow",
       "Action": "s3:*",
-      "Resource": "arn:aws:s3:::my-bucket/*",
+      "Resource": "arn:aws:s3:::iaaciam/*",
       "Condition": {
         "IpAddress": {
           "aws:SourceIp": "${data.http.ip.body}"
@@ -46,7 +46,7 @@ EOF
 }
 
 # Update the bucket policy with the IAM policy created in the previous step
-resource "aws_s3_bucket_policy" "my_policy" {
-  bucket = "my-bucket"
+resource "aws_s3_bucket_policy" "iaaciamp" {
+  bucket = "iaaciam1"
   policy = "${aws_iam_policy.my_policy.arn}"
 }
